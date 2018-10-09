c6d3c193f688291daa0bed0cbcb77f3abf91eb4c
NinhThiLien <lienbkak59@gmail.com>
2018-10-10 02:49:29 +0700

    login+logout+advance

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   	user = User.find_by(login_name: params[:session][:login_name].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
     	flash[:danger] = 'Invalid login name/password combination'
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  	log_out
+  	log_out if logged_in?
     redirect_to root_url
   end
 end
