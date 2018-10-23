89a113b697228223263035650a455164a95bcd86
hoangtuyb96 <hoangtuyb96@gmail.com>
2018-10-23 14:31:24 +0700

    edit_book

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :books
+  resources :books do
+    get "change_form"
+  end
   resources :users
   resources :account_activations, only: [:edit]
 end
