80a2a3c83541d2b0db5275c261654de5fc03197c
hoangtuyb96 <hoangtuyb96@gmail.com>
2018-11-11 22:30:51 +0700

    delete_comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :find_book, only: :create
+  before_action :find_comment, only: :destroy
 
   def create
     @comments = book.comments
@@ -12,12 +13,23 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @book = comment.book
+    if comment.destroy
+      redirect_to book
+    end
+  end
+
   private
 
   attr_reader :book, :comment
 
   def find_book
     @book = Book.find_by id: params[:book_id]
+  end
+
+  def find_comment
+    @comment = Comment.find_by id: params[:id]
   end
 
   def comment_params
