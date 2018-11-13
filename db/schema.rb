897e9a3c7f695b8d2dcd9d269662b9c433ed90ec
Nguyen Tuan Linh <chaosmeteor96@gmail.com>
2018-11-13 07:38:10 +0000

    fix pagination css

--- db/schema.rb
+++ db/schema.rb
@@ -113,7 +113,7 @@ ActiveRecord::Schema.define(version: 2018_11_12_023344) do
     t.datetime "updated_at", null: false
     t.string "remember_digest"
     t.string "activation_digest"
-    t.boolean "activated", default: false
+    t.boolean "activated", default: true
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
