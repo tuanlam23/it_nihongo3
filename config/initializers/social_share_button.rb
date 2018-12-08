290af355801142181395db918ef2c3e5c4305665
hoangtuyb96 <hoangtuyb96@gmail.com>
2018-12-08 22:34:59 +0700

    fix_login_with_gg

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus tumblr pinterest email)
+  config.allow_sites = %w(twitter google_plus pinterest email)
 end
